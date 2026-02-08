8ca71b275889364216e85ead6bd5ec7245372d07
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-02-08 12:46:29 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "BURAYA",
-  authDomain: "BURAYA",
-  databaseURL: "BURAYA",
-  projectId: "BURAYA",
-  storageBucket: "BURAYA",
-  messagingSenderId: "BURAYA",
-  appId: "BURAYA"
+  apiKey: "AIzaSyCAzffyBznJFzt9-mixht5NGoLLFpomXE8",
+  authDomain: "diapp-3a705.firebaseapp.com",
+  projectId: "diapp-3a705",
+  storageBucket: "diapp-3a705.firebasestorage.app",
+  messagingSenderId: "681461127191",
+  appId: "1:681461127191:web:cf762e58807aeb70d36452"
 };
 
 const app = initializeApp(firebaseConfig);
